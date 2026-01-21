37f32640d020d5b0634c280f5a2207a6cc2fa025
jao <joaopedro.fariamarins@gmail.com>
2026-01-21 17:49:53 -0300

    sistema de estoque contendo 6 funcionalidades, incluindo adicionar, listar, remover, buscar e somar produtos

--- treinofuncao.py
+++ treinofuncao.py
@@ -1,44 +1,61 @@
-alunos = [
-    {"nome": "João", "nota": 8.5},
-    {"nome": "Maria", "nota": 5.5},
-    {"nome": "Pedro", "nota": 3.0},
-    {"nome": "Ana", "nota": 7.0},
-    {"nome": "Carlos", "nota": 6.0},
-    {"nome": "Beatriz", "nota": 9.5},
-    {"nome": "Lucas", "nota": 4.5},
-    {"nome": "Fernanda", "nota": 7.8},
-    {"nome": "Rafael", "nota": 6.5},
-    {"nome": "Juliana", "nota": 2.5}
-]
+produtos = [
+    {"nome": "Mouse", "preco": 50.00, "quantidade": 10},
+    {"nome": "Teclado", "preco": 120.00, "quantidade": 5},
+    {"nome": "Monitor", "preco": 800.00, "quantidade": 3}
+    ]
+valor_total = 0
+encontrado = False
+while True:
+    print(f"=== SISTEMA DE ESTOQUE === \n 1 - Adicionar produto \n 2 - Listar produtos \n 3 - Buscar produto \n 4 - Atualizar quantidades \n 5 - Remover produtos sem estoque \n 6 - Valor total do estoque \n 0 - Sair")
+    escolha = input('Escolha uma opção: ')
 
-aprovados = []
-reprovados = []
-recuperacao = []
+    if escolha == '1':
+        print("ADICIONANDO NOVO PRODUTO")
+        nome_pa = input('Qual produto deseja adicionar: ')
+        preco_pa = float(input('Qual o preço do produto: '))
+        qtd_pa = int(input('Qual a quantidade do produto: '))
+        pa = {"nome": nome_pa, "preco": preco_pa, "quantidade": qtd_pa}
+        produtos.append(pa)
 
-def classificar_alunos(lista):
-    for aluno in lista:
-        if aluno["nota"] >= 7:
-            aprovados.append(aluno)
-        elif aluno["nota"] >= 5:
-            recuperacao.append(aluno)
-        else:
-            reprovados.append(aluno)
-    print(f"APROVADOS ({len(aprovados)})")
-    for aluno in aprovados:
-           
-        print()
-        print(f" {aluno['nome']} ({aluno['nota']})")
-    print(f"RECUPERACAO ({len(recuperacao)})")
-    for aluno in recuperacao:
+        
+    elif escolha == '0':
+        exit()
+    
+    elif escolha == '2':
+        for produto in produtos:
+            print(f"PRODUTO -- {produto['nome']} | PREÇO -- {produto['preco']} | QTD -- {produto['quantidade']} \n")
+    
+    elif escolha == '3':
+        pesquisado = input('Qual produto deseja encontrar: ')
+        for produto in produtos:
+        
             
-        print()
-        print(f" {aluno['nome']} ({aluno['nota']})")
-    print(f"REPROVADOS ({len(reprovados)})")
-    for aluno in reprovados:
+            if produto['nome'].lower() == pesquisado.lower():
+                print(f"Produto encontrado! \n {produto['nome']} -- {produto['preco']} -- {produto['quantidade']}")
+                break
             
-        print()
+        if not encontrado:
+            print('Produto não encontrado')
 
-        print(f" {aluno['nome']} ({aluno['nota']})")
+    elif escolha == '4':
+        nome_upd_qtd = input('Qual produto deseja alterar a quantidade: ').lower()
+        upd_qtd = int(input('Qual a nova quantidade: '))
+        for produto in produtos:
+            
+            if produto['nome'].lower() == nome_upd_qtd:
+                produto['quantidade'] = upd_qtd
+                print('Valor alterado com sucesso!')
+                encontrado = True
+                break
+        if not encontrado:
+            print('Produto não encontrado')    
 
+    elif escolha == '5':
+    
+        produtos = [p for p in produtos if p['quantidade'] > 0]
+        print('Produtos sem estoque removidos!')
+    
+    elif escolha == '6':
 
-classificar_alunos(alunos)
+        valor_total = sum(p['preco']*p['quantidade'] for p in produtos)
+        print(valor_total)
