6521c46f0ef4d636ec50db8a76f6abcf221759e2
jao <joaopedro.fariamarins@gmail.com>
2026-01-21 01:08:27 -0300

    verifica quantas palavras iniciam com vogal

--- treinofuncao.py
+++ treinofuncao.py
@@ -1,6 +1,21 @@
-def contar_tarefas(lista):
-    return len(lista)
+resposta = 0
+def inicia_com_vogal(lista):
 
-tarefas = ['b', 'c', 'a']
+    contagem = 0
+    for palavra in lista:
+        primeira_letra = palavra[0].lower()
+        if primeira_letra in 'aeiou':
+            contagem += 1
+        
+        
+    return contagem
 
-print(contar_tarefas(tarefas))
+
+palavras = ['anel', 'abelha', 'zoiao']
+
+resultado = inicia_com_vogal(palavras)
+
+if resultado != 0:
+    print(resultado)
+else:
+    print('Lista vazia')
