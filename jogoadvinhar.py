5da6df0e2b3ccec780b512440480613ce0d9c31b
jao <joaopedro.fariamarins@gmail.com>
2026-01-20 23:25:04 -0300

    adicionei numero de tentativas

--- jogoadvinhar.py
+++ jogoadvinhar.py
@@ -4,11 +4,11 @@ print('=== JOGO DE ADVINHACAO ===')
 
 numeropensado = random.randint(1,100)
 numtentado = 0
-
+tentativas = 0
 while numtentado != numeropensado:
     try:
         numtentado = int(input('Digite um número: '))
-        
+        tentativas += 1
 
         if numtentado > numeropensado:
             print('Número maior que o pensado!')
@@ -18,6 +18,6 @@ while numtentado != numeropensado:
         print('Isso não é um número!')
     
 
-
+tentativasstr = str(tentativas)
 if numtentado == numeropensado:
-    print('Parabéns, você acertou!!')
+    print('Parabéns, você acertou em '+ tentativasstr+ ' tentativas!!' )
